2433e54b531d7c5a5ec83caf615720d7a42d748f
pump9918 <hoyeon9918@gmail.com>
2023-12-15 21:56:29 +0900

    [feat] #0 settings.py static url 수정

--- config/settings.py
+++ config/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static') # 배포용 static 파일 모으기
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # media폴더를 만들어 파일 모아줌
